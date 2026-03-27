795e0193d12624247c65b605ecd7a5e2153ce301
Nathan Catledge <nathancatledge@Nathans-MacBook-Air-667.local>
2026-03-26 23:26:08 -0600

    Commit local changes before merge

--- server.js
+++ server.js
@@ -144,6 +144,8 @@ User: ${message}
   }
 });
 
-app.listen(3000, () => console.log("API listening on port 3000"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
 
 //run local with node server.js
